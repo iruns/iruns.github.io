1cbf58cb21a611424d6768b53b37bee829200b42
iruns <irun.siregar@gmail.com>
2023-04-04 01:59:45 +0700

    fixed the gulp file, and actually updated ourch-3

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
-const { src, symlink, series } = require('gulp')
+const { src, dest, series } = require('gulp')
+const clean = require('gulp-dest-clean')
+const rimraf = require('gulp-rimraf')
 const run = require('gulp-run')
 
 const source = 'C:/Data/Werk/MoneyMaking/Tujusemesta/Ourchetype/ourchetype-3/ourchetype-3'
+const destination = 'ourchetype-3'
 
-function link() {
-  return src(source + '/dist/*')
-    .pipe(symlink('ourchetype-3/'))
-}
 function build() {
   return run('npm --prefix ' + source + ' run build').exec()
 }
+function update() {
+  return src(source + '/dist/**')
+    .pipe(clean(destination))
+    .pipe(dest(destination))
+}
 
-exports.ourch3 = series(link, build)
+exports.ourch3 = series(build, update)
