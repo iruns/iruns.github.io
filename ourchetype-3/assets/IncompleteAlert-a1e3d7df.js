538d5c9585d15f64bbcb257489c5d879c8c85534
iruns <irun.siregar@gmail.com>
2023-04-23 22:01:18 +0700

    progress guard (etc) done

---
+++ ourchetype-3/assets/IncompleteAlert-a1e3d7df.js
@@ -0,0 +1,3 @@
+import{e,o as t,a as s,d as a,t as o,_}from"./progress-0ab57a67.js";const n={class:"incomplete-alert"},c={class:"alert"},l=e({__name:"IncompleteAlert",setup(r){return(p,d)=>(t(),s("div",n,[a("h2",c,o(`Proses belum lengkap.
+Coba selesaikan
+proses terlebih dahulu`),1)]))}});const m=_(l,[["__scopeId","data-v-ad2e0a45"]]);export{m as I};
