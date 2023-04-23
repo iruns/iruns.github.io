53f72a82ac45a7e2e49acd1d5003288fc36c82a9
iruns <irun.siregar@gmail.com>
2023-04-23 22:22:12 +0700

    uupdated gulp to include ourchetype-3-dist

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const run = require('gulp-run')
 
 const source = 'C:/Data/Werk/MoneyMaking/Tujusemesta/Ourchetype/ourchetype-3/ourchetype-3'
 const destination = 'ourchetype-3'
+const destination2 = 'C:/Data/Werk/MoneyMaking/Tujusemesta/Ourchetype/ourchetype-3/ourchetype-3-dist'
 
 function build() {
   return run('npm --prefix ' + source + ' run build').exec()
@@ -12,6 +13,9 @@ function update() {
   return src(source + '/dist/**')
     .pipe(clean(destination))
     .pipe(dest(destination))
+    .pipe(clean(destination2, undefined, { force: true }))
+    .pipe(dest(destination2))
 }
 
 exports.ourch3 = series(build, update)
+exports.ourch3_update = update
