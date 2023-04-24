b68e37b9a8908e71f14cc2b7cf754d7e697ea440
iruns <irun.siregar@gmail.com>
2023-04-25 01:00:27 +0700

    added logo, updated outro passcode

--- ourchetype-3/assets/passcodes-5f207fe9.js
+++
@@ -1,1 +0,0 @@
-import{a8 as a}from"./progress-7b0ac2aa.js";const r={[a.Intro]:"",[a.Persona]:"area1",[a.Shadow]:"area2",[a.Self]:"area3",[a.Outro]:"outro"};export{r as p};
---
+++ ourchetype-3/assets/passcodes-c5e0f9a0.js
@@ -0,0 +1,1 @@
+import{a8 as a}from"./progress-7b0ac2aa.js";const r={[a.Intro]:"",[a.Persona]:"area1",[a.Shadow]:"area2",[a.Self]:"area3",[a.Outro]:"yourfusion"};export{r as p};
