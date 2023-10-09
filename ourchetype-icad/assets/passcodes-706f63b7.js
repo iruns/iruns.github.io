449c06bc3e4e12ae9b125e8e903a5a6e868d30db
iruns <irun.siregar@gmail.com>
2023-10-10 02:11:28 +0700

    icad almost done

--- ourchetype-icad/assets/passcodes-706f63b7.js
+++
@@ -1,1 +0,0 @@
-import{A as a}from"./index-3a343bb9.js";const s={[a.Intro]:"",[a.Masks]:"",[a.Persona]:"meja2",[a.Shadow]:"meja3",[a.Self]:"meja4",[a.Outro]:""};export{s as p};
---
+++ ourchetype-icad/assets/passcodes-a3a0a949.js
@@ -0,0 +1,1 @@
+import{A as a}from"./index-27c25e34.js";const s={[a.Intro]:"",[a.Masks]:"",[a.Persona]:"terang",[a.Shadow]:"gelap",[a.Self]:"cahaya",[a.Outro]:""};export{s as p};
