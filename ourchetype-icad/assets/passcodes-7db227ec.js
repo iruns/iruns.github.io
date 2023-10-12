54844ad10435f82b58bdf95e15b9eddadf0317b9
iruns <irun.siregar@gmail.com>
2023-10-12 20:05:37 +0700

    split result items

---
+++ ourchetype-icad/assets/passcodes-7db227ec.js
@@ -0,0 +1,1 @@
+import{A as a}from"./index-8a6f6b7e.js";const s={[a.Intro]:"",[a.Masks]:"",[a.Persona]:"terang",[a.Shadow]:"gelap",[a.Self]:"cahaya",[a.Outro]:""};export{s as p};
--- ourchetype-icad/assets/passcodes-d2516274.js
+++
@@ -1,1 +0,0 @@
-import{A as a}from"./index-0b93edf1.js";const s={[a.Intro]:"",[a.Masks]:"",[a.Persona]:"terang",[a.Shadow]:"gelap",[a.Self]:"cahaya",[a.Outro]:""};export{s as p};
